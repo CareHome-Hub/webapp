04035e4e1ff6863032d232300371b63d6564f58d
spanarchie <spanarchian@gmail.com>
2018-09-09 23:32:10 +0100

    ADDED graph

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, Response
+from py2neo import Graph
+
+g = Graph('bolt://localhost:7687')
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route("/")
+def hello():
+    df = g.run(
+        "MATCH (a :company) RETURN a.company_id as company_id, a.name as name, a.industry as industry  LIMIT 4").data()
+    print("\n\nDF = {}s".format(df))
+    return Response("Hi from your Flask app running in your Docker container! {}".format(df))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
