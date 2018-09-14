187113d822f4b42a77aebd5d7d9e35c645c70709
Colin Moore-Hill <colinmoore-hill@ip-192-168-1-235.eu-west-1.compute.internal>
2018-09-14 17:43:42 +0100

    ADDED: web

---
+++ web/data/models/touchpoints.py
@@ -0,0 +1,59 @@
+touchpoints = [
+  {
+    "touchpoint": "Touchpoint1",
+    "conversion_rate": 6.35,
+    "campaign_cost": 100,
+    "sellers_cost": 25.00,
+    "sellers_cost_updates": 2.50
+  },
+  {
+    "touchpoint": "Touchpoint2",
+    "conversion_rate": 6.35,
+    "campaign_cost": 100,
+    "sellers_cost": 25.00,
+    "sellers_cost_updates": 2.50
+  },
+  {
+    "touchpoint": "Touchpoint3",
+    "conversion_rate": 6.35,
+    "campaign_cost": 100,
+    "sellers_cost": 25.00,
+    "sellers_cost_updates": 2.50
+  },
+  {
+    "touchpoint": "Touchpoint4",
+    "conversion_rate": 6.35,
+    "campaign_cost": 100,
+    "sellers_cost": 25.00,
+    "sellers_cost_updates": 2.50
+  },
+  {
+    "touchpoint": "Touchpoint5",
+    "conversion_rate": 6.35,
+    "campaign_cost": 100,
+    "sellers_cost": 25.00,
+    "sellers_cost_updates": 2.50
+  },
+  {
+    "touchpoint": "Touchpoint6",
+    "conversion_rate": 6.35,
+    "campaign_cost": 100,
+    "sellers_cost": 25.00,
+    "sellers_cost_updates": 2.50
+  },
+  {
+    "touchpoint": "Touchpoint7",
+    "conversion_rate": 6.35,
+    "campaign_cost": 100,
+    "sellers_cost": 25.00,
+    "sellers_cost_updates": 2.50
+  }
+]
+
+
+def get_touchpoints():
+    return touchpoints
+
+def get_select_touchpoints(reqd=[]):
+    resp = {}
+    
